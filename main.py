a435305c8d07c57762363bf307f2950fd5be9aa4
Kris <89760800+KrisJD@users.noreply.github.com>
2022-11-02 08:52:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -505,6 +505,6 @@ async def 학사일정(ctx):
 async def 정보(ctx):
     await ctx.send('봇 정보 : 구동 체제 - VScode (Python) 버전 : 3.10.8 *기반 : project POPPY (discord.py : 2.0.1')
 
-TOKEN = os.environ["TOKEN"]
+TOKEN = str(os.environ["TOKEN"])
 
 bot.run(TOKEN)
