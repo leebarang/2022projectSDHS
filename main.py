79b4ee9f39c24afbd3dc59d113850e91aeffc9b1
Kris <89760800+KrisJD@users.noreply.github.com>
2022-11-02 09:21:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -508,5 +508,6 @@ async def 정보(ctx):
     await ctx.send('봇 정보 : 구동 체제 - VScode (Python) 버전 : 3.10.8 *기반 : project POPPY (discord.py : 2.0.1')
 
 TOKEN = os.getenv("BOT_TOKEN")
-
+load_dotenv()
 bot.run(TOKEN)
+print(TOKEN)
